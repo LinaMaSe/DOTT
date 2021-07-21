8c5488473011e2379c2741d05745f199e5e81f1d
Carolina Martinez <74791614+LinaMaSe@users.noreply.github.com>
2021-07-21 18:17:08 -0500

    execute sonar-scaner

---
+++ cidr_convert_api/python/sonar.sh
@@ -0,0 +1,5 @@
+sonar-scanner \
+                -Dsonar.projectKey=codeAnalysis \
+                -Dsonar.sources=. \
+                -Dsonar.host.url=http://ec2-18-221-253-127.us-east-2.compute.amazonaws.com:9000 \
+                -Dsonar.login=f73e0ac4d8aaf4fdd588047144df5ce03f04b06c
