c136351d3f17d836cda2461433a1a05acf6b9fe1
Carolina Martinez <74791614+LinaMaSe@users.noreply.github.com>
2021-07-22 11:49:08 -0500

    sonar updated

--- cidr_convert_api/python/sonar.sh
+++ cidr_convert_api/python/sonar.sh
@@ -1,6 +1,5 @@
 sonar-scanner \
-                -Dsonar.projectKey=codeAnalysis \
-                -Dsonar.sources=. \
-                -Dsonar.host.url=http://18.117.184.134:9000/ \
-                -Dsonar.username=admin
-                -Dsonar.password=admin
+  -Dsonar.projectKey=codeAnalysis \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://ec2-18-117-184-134.us-east-2.compute.amazonaws.com:9000 \
+  -Dsonar.login=f73e0ac4d8aaf4fdd588047144df5ce03f04b06c
