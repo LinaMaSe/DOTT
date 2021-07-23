14f434c782b9ea434f0e3733945c4679f164b2e0
Carolina Martinez <74791614+LinaMaSe@users.noreply.github.com>
2021-07-23 12:43:18 -0500

    new project to analyze

--- cidr_convert_api/python/sonar.sh
+++ cidr_convert_api/python/sonar.sh
@@ -1,5 +1,5 @@
 sonar-scanner \
-  -Dsonar.projectKey=codeAnalysis \
+  -Dsonar.projectKey=CodeTest \
   -Dsonar.sources=. \
   -Dsonar.host.url=http:18.117.184.134:9000 \
   -Dsonar.login=f73e0ac4d8aaf4fdd588047144df5ce03f04b06c
