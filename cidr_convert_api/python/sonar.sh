e39b3d8f98f928a533662da1dcb7e79191cdbbe6
Carolina Martinez <74791614+LinaMaSe@users.noreply.github.com>
2021-07-23 03:35:25 -0500

    mas time out a sonar

--- cidr_convert_api/python/sonar.sh
+++ cidr_convert_api/python/sonar.sh
@@ -3,3 +3,4 @@ sonar-scanner \
   -Dsonar.sources=. \
   -Dsonar.host.url=http:18.117.184.134:9000 \
   -Dsonar.login=f73e0ac4d8aaf4fdd588047144df5ce03f04b06c
+  -Dorg.jenkinsci.plugins.durabletask.BourneShellScript.HEARTBEAT_CHECK_INTERVAL=86400
